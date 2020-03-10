0b9b184055f3ec7e63c466d3fbc3b8398a4d6506
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2020-03-10 11:58:38 +0200

    SPCXCFZB-666 / Home and About alts

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { style } from '@angular/animations';
 
 @Component({
     selector: 'jhi-footer',
-    templateUrl: './footer.component.html'
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.css']
 })
-export class FooterComponent {}
+export class FooterComponent {
+    private partnerImages = ['usaid.png', 'spc.png', 'forum.png', 'giz.png', 'ausaid.png', 'sprep.svg'];
+}
